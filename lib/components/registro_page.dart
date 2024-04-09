d7ad080eae1c0a7063e45d539ae0157646648999
Silas Eduardo Rodrigues dos Santos <01745984402@control.govrn>
2024-04-09 13:55:35 -0300

    Mudando a pagina de cadastro

--- lib/components/registro_page.dart
+++ lib/components/registro_page.dart
@@ -7,41 +7,83 @@ class RegistroPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cadastro'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(labelText: 'Nome'),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(labelText: 'Email'),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(labelText: 'Senha'),
-                obscureText: true,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Adicione a lógica para criar a conta aqui
-                },
-                child: Text('Cadastrar'),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Navega de volta para a página de login
-                  Navigator.pop(context);
-                },
-                child: Text('Voltar'),
-              ),
-            ],
-          ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Nome'),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Senha'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Confirme sua senha'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Endereço',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'CEP'),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Rua'),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Número'),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Complemento'),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Cidade'),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Estado (UF)'),
+              maxLength: 2,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Adicione a lógica para criar a conta aqui
+              },
+              child: Text('Cadastrar'),
+            ),
+            SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                // Navega de volta para a página de login
+                Navigator.pop(context);
+              },
+              child: Text('Voltar'),
+            ),
+          ],
         ),
       ),
     );
